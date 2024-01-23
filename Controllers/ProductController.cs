5c0fb63c30da5cb823e9b2257c3912a6c1003624
Cesar <cesarhenrique_prado@hotmail.com>
2024-01-23 11:01:44 -0300

    feat/ Criação e configuração da controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using ApiProduct.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProduct.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return new List<Product>();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,23 +2,18 @@ using ApiProduct.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = "DefaulConection";
-
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseSqlite(connectionString);
+    options.UseSqlite(connectionString:"DataSource=app.db;Cache=Shared");
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
