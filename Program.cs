7ba1eb4e61178a3aaebd4a8a30cf44b60ba1aadf
César Prado <cesarhenrique_prado@hotmail.com>
2024-01-25 09:55:31 -0300

    feat: added the serilog tool

--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
 using ApiProduct.Data;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//Configure Serilog
+Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Debug()
+        .WriteTo.Console()
+        .WriteTo.File("log.txt")
+        .CreateLogger();
+
+
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.ClearProviders();
+    loggingBuilder.AddSerilog();
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
